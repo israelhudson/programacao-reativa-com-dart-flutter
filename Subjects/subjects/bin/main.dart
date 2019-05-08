23611c44487f4c6b9b5d8ae0a46f662ce4bbd7e2
israelhudson <israelhudson@gmail.com>
2019-05-08 00:24:54 -0300

    StreamSubscription

--- Subjects/subjects/bin/main.dart
+++ Subjects/subjects/bin/main.dart
@@ -1,34 +1,18 @@
+import 'dart:async';
+
 import 'package:rxdart/rxdart.dart';
 
 main() {
 
-  //pubSubject();  
-  //behSubject();
-  replaySubject();
-}
+  Observable observavel = Observable.just("a");
+  observavel.listen(print);
 
-replaySubject(){//Recupera todo o historico de dados adicionado
-  ReplaySubject r = new ReplaySubject();
-  r.sink.add(10);
-  r.sink.add(15);
-  r.sink.add(20);
-
-  r.stream.listen((data) => print("print $data"));
-  print("values: ${r.values}");
-}
-
-behSubject(){//pega o ultimo dado adicionado
   BehaviorSubject b = new BehaviorSubject();
+
   b.sink.add(15);
-  b.stream.listen(print);
-  print(b.value);
+
+  StreamSubscription subscription = b.listen(print);
+  subscription.onData((data) => print("Mudei o método $data"));
+  
 }
 
-pubSubject(){//adciona dado no buffer
-  PublishSubject p = new PublishSubject();
-
-  p.stream.listen(print);
-  p.sink.add(1);
-  p.sink.add(2);
-  p.sink.add(3);
-}
