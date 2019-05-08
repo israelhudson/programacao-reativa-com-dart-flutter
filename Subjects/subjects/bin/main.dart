944eb6701ce68a2dd49b1127f88ab21f398babc8
israelhudson <israelhudson@gmail.com>
2019-05-07 22:47:06 -0300

    Replay Subject

--- Subjects/subjects/bin/main.dart
+++ Subjects/subjects/bin/main.dart
@@ -3,17 +3,28 @@ import 'package:rxdart/rxdart.dart';
 main() {
 
   //pubSubject();  
-  behbSubject();
+  //behSubject();
+  replaySubject();
 }
 
-behbSubject(){
+replaySubject(){//Recupera todo o historico de dados adicionado
+  ReplaySubject r = new ReplaySubject();
+  r.sink.add(10);
+  r.sink.add(15);
+  r.sink.add(20);
+
+  r.stream.listen((data) => print("print $data"));
+  print("values: ${r.values}");
+}
+
+behSubject(){//pega o ultimo dado adicionado
   BehaviorSubject b = new BehaviorSubject();
   b.sink.add(15);
   b.stream.listen(print);
   print(b.value);
 }
 
-pubSubject(){
+pubSubject(){//adciona dado no buffer
   PublishSubject p = new PublishSubject();
 
   p.stream.listen(print);
