5f68af66aea74c72e93af824cb97b0e02b3ccce8
israelhudson <israelhudson@gmail.com>
2019-06-17 22:56:52 -0300

    SKIP (ignora eventos)

--- Subjects/subjects/bin/main.dart
+++ Subjects/subjects/bin/main.dart
@@ -5,8 +5,8 @@ import 'package:rxdart/rxdart.dart';
 main() {
 
   BehaviorSubject<String> b = new BehaviorSubject<String>();
-  //Mostra apenas 3 dados iniciais da lista
-  b.stream.take(3).listen(print);
+  //ignora apenas 3 dados iniciais da lista
+  b.stream.skip(3).listen(print);
   b.sink.add("1");
   b.sink.add("2");
   b.sink.add("3");
